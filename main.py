16761e69af31833a71685ae13de6c9594855ee7e
Dinho <39270503+Eude06@users.noreply.github.com>
2023-06-27 22:02:59 +0200

    Create main.py
    Included the first lines of my python code to the project.

---
+++ main.py
@@ -0,0 +1,4 @@
+#Welcome viewers to your page. 
+print("Welcome to my page and thank you for taking the time to view the page! ")
+name = input("Please introduce yourself by telling me what your name is?\n")
+print("Nice to meet you " + name)
